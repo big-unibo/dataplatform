813ae776193919497877f5a83aed960a165a3135
Manuele Pasini <manuele.pasini@unibo.it>
2024-01-08 15:59:47 +0000

    refactoring

--- multiple_stacks/deploy-swarm.sh
+++ multiple_stacks/deploy-swarm.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -o allexport
-source .env
+source ./../.env
 set +o allexport
 
 cp -f .env ../dataplatform/conf_files/
 
 # HDFS Stack
-#envsubst < docker-compose-hdfs.yaml > runtime/docker-compose-hdfs-subs.yaml
-#docker stack deploy -c runtime/docker-compose-hdfs-subs.yaml DataPlatform-HDFS
+envsubst < docker-compose-hdfs.yaml > runtime/docker-compose-hdfs-subs.yaml
+docker stack deploy -c runtime/docker-compose-hdfs-subs.yaml DataPlatform-HDFS
 
 # YARN stack
-#envsubst < docker-compose-yarn.yaml > runtime/docker-compose-yarn-subs.yaml
-#docker stack deploy -c runtime/docker-compose-yarn-subs.yaml DataPlatform-YARN
+envsubst < docker-compose-yarn.yaml > runtime/docker-compose-yarn-subs.yaml
+docker stack deploy -c runtime/docker-compose-yarn-subs.yaml DataPlatform-YARN
 
 # Spark stack
-#envsubst < docker-compose-spark.yaml > runtime/docker-compose-spark-subs.yaml
-#docker stack deploy -c runtime/docker-compose-spark-subs.yaml DataPlatform-Spark
-#
+envsubst < docker-compose-spark.yaml > runtime/docker-compose-spark-subs.yaml
+docker stack deploy -c runtime/docker-compose-spark-subs.yaml DataPlatform-Spark
+
 # ZooKeper stack
-#envsubst < docker-compose-zookeper.yaml > runtime/docker-compose-zookeper-subs.yaml
-#docker stack deploy -c runtime/docker-compose-zookeper-subs.yaml DataPlatform-ZooKeper
+envsubst < docker-compose-zookeper.yaml > runtime/docker-compose-zookeper-subs.yaml
+docker stack deploy -c runtime/docker-compose-zookeper-subs.yaml DataPlatform-ZooKeper
 
 # Kafka stack
 envsubst < docker-compose-kafka.yaml > runtime/docker-compose-kafka-subs.yaml
