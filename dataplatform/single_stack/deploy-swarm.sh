7b67003969690809c59568f9fd1851028630f189
Manuele Pasini <manuele.pasini@unibo.it>
2024-01-09 13:31:25 +0000

    folder refactoring

--- dataplatform/single_stack/deploy-swarm.sh
+++ dataplatform/single_stack/deploy-swarm.sh
@@ -4,7 +4,7 @@ source ./../.env
 set +o allexport
 
 cp -f .env ./dataplatform/conf_files/
-envsubst < docker-compose.yaml > docker-compose-subs.yaml
+envsubst < docker-compose.yaml > runtime/docker-compose-subs.yaml
 
 #envsubst < docker-compose-swarm.yaml > docker-compose-sub.yaml
-docker stack deploy -c docker-compose-subs.yaml myTestStack
+docker stack deploy -c runtime/docker-compose-subs.yaml myTestStack
