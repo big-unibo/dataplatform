c70a5d3115fa0e475b446c89d1c1f84555a36029
Manuele Pasini <manuele.pasini@unibo.it>
2024-01-09 13:31:25 +0000

    folder refactoring

--- dataplatform/rm_volumes.sh
+++ dataplatform/rm_volumes.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-DOCKER_COMPOSE_FILE="docker-compose-subs.yaml"
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <stack_name> <docker_compose_file>"
+    exit 1
+fi
+
+STACK_NAME="$1"
+DOCKER_COMPOSE_FILE="$2"
 
 VOLUME_NAMES=$(python3 -c "import yaml; f = open('$DOCKER_COMPOSE_FILE'); data = yaml.safe_load(f); f.close(); volumes = data.get('volumes', {}); print('\n'.join(volumes.keys()))")
 
-stack=${1:-}
 for VOLUME_NAME in $VOLUME_NAMES; do
-    echo "removing $stack""_$VOLUME_NAME"
-    docker volume rm $stack""_$VOLUME_NAME
+    echo "removing $STACK_NAME""_$VOLUME_NAME"
+    docker volume rm $STACK_NAME""_$VOLUME_NAME
 done
 
 echo "Every volume in $DOCKER_COMPOSE_FILE has been removed."
