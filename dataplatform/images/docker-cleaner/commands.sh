f77c1303c5b720df99f6ba9d49e96aedecc4a77d
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-06-26 16:39:53 +0200

    fix commands script

--- dataplatform/images/docker-cleaner/commands.sh
+++ dataplatform/images/docker-cleaner/commands.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Remove all stopped containers
 echo "Removing all stopped containers..."
-docker container prune -f+
-# Remove all dangling images
+docker container prune -f
+
 echo "Removing all dangling images..."
 docker image prune -f
-# Remove all dangling volumes
-echo "Removing all dangling volumes..."
+
+echo "Removing cache..."
 docker builder prune -f
 echo "Docker cleanup complete."
