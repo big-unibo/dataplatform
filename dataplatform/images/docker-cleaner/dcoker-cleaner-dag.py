d08854aae1a7a91b649ec0b0f3dd7c415a5ecff6
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-06-26 15:55:47 +0200

    update cleaner

--- dataplatform/images/docker-cleaner/dcoker-cleaner-dag.py
+++ dataplatform/images/docker-cleaner/dcoker-cleaner-dag.py
@@ -7,7 +7,7 @@ import sys
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 3, 21)
+    'start_date': datetime(2024, 6, 24)
 }
 
 dag = DAG('utils_docker_cleaner', default_args=default_args,
