83cdb3b73f54007218233977138d8058127bd35e
manuelepasini <manuele.pasini@unibo.it>
2024-09-11 08:31:59 +0000

    feat: Swarm cleaner now also cleans up dangling services

--- dataplatform/images/docker-cleaner/commands.sh
+++ dataplatform/images/docker-cleaner/commands.sh
@@ -9,3 +9,15 @@ docker image prune -f
 echo "Removing cache..."
 docker builder prune -f
 echo "Docker cleanup complete."
+
+
+echo "Cleaning up old services left around the Swarm..."
+
+docker service ls --format "{{.ID}} {{.Replicas}}" | while read service_id replicas; do
+  # Check if the replication factor starts with 0 (i.e., 0/x)
+  if [[ "$replicas" =~ ^0/ ]]; then
+    echo "Removing service: $service_id"
+    docker service rm $service_id
+  fi
+done
+echo "Succesfully cleaned Swarm services"
