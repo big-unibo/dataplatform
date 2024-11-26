c0caa4283c113affbc9d5223331e290b389d8f8b
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-11-26 17:43:53 +0100

    fix build image, push must have tags for remove the images with registry apis

--- dataplatform/images/build_image.sh
+++ dataplatform/images/build_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 VERSION=v0.0.1
-IMAGE_NAME=127.0.0.0:5000/$1:$VERSION
+REGISTRY_PATH=127.0.0.0:5000/
+IMAGE_NAME=$1:$VERSION
 docker image build --tag $IMAGE_NAME -f ./$1/Dockerfile .
-docker push $IMAGE_NAME
+docker tag $IMAGE_NAME $REGISTRY_PATH$IMAGE_NAME
+docker push $REGISTRY_PATH$IMAGE_NAME
